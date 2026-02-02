cbd8038f86a32f70e31168753ad65b6e1274398b
Axm.pachara <pachared.amr12@gmail.com>
2026-02-02 21:29:33 +0700

    ทำหน้า App.jsx เป็นหน้าหลักเพื่อสามารถไปยังหน้าอื่นๆได้

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,18 @@
-import React from 'react'
-import DocsPage from './components/DocsPage'
+import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import DocsPage from "./pages/DocsPage";
+import MainLayout from "./layouts/MainLayout";
 
 function App() {
   return (
-    <div>
-      <DocsPage />
-    </div>
-  )
+    <Routes>
+      <Route element={<MainLayout />}>
+        <Route path="/" element={<Home />} />
+        <Route path="/docs/:docId" element={<DocsPage />} />
+      </Route>
+    </Routes>
+  );
 }
 
-export default App
+export default App;
