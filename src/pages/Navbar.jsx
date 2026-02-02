9f78786e4eb3776a60811a75b23b432334dac083
Axm.pachara <pachared.amr12@gmail.com>
2026-02-03 01:52:41 +0700

    ปรับพื้นหลังของ Navbar ให้เป็นแบบพื้นหลังเบลอและทำให้ Logo เวลา hover ขยับเล็กน้อยแบบ Animation

--- src/pages/Navbar.jsx
+++ src/pages/Navbar.jsx
@@ -3,12 +3,28 @@ import PCR from "../assets/PCR.svg";
 
 function Navbar() {
   return (
-    <nav className="sticky top-5 z-50 mx-4 bg-[#0B0B10]/70 backdrop-blur-lg supports-backdrop-filter:bg-[#0B0B10]/60 border border-white/10 rounded-2xl">
+    <nav className="sticky top-5 z-50 mx-4 xl:mx-110 bg-[#0B0B10]/70 backdrop-blur-lg supports-backdrop-filter:bg-[#0B0B10]/60 border border-[#1f2330] rounded-2xl">
       <div className="max-w-6xl mx-auto px-4 py-3 flex items-center justify-between">
-        <Link to="/" className="text-xl font-bold text-gray-900">
-          <img src={PCR} alt="PCR LOGO" className="h-8" />
+        {/* Logo + Title */}
+        <Link to="/" className="group flex items-center gap-3">
+          <img
+            src={PCR}
+            alt="PCR LOGO"
+            className="
+                h-8
+                transition-all duration-300 ease-out
+                group-hover:scale-110
+                group-hover:rotate-6
+                group-hover:drop-shadow-[0_0_8px_rgba(59,130,246,0.6)]
+                "
+          />
+
+          <span className="text-white font-semibold tracking-wide">
+            บทความพชร
+          </span>
         </Link>
 
+        {/* Menu */}
         <div className="flex gap-6 text-gray-500">
           <NavLink
             to="/"
