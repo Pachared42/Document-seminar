e8729864ac0b2701ba3cad58322bc66f99628caa
Axm.pachara <pachared.amr12@gmail.com>
2026-02-04 00:11:45 +0700

    เปลี่ยนชื่อ title จาก Portfolio Seminar + Deploy ขึ้น Vercel เป็น  Devfolio Seminar + Deploy ขึ้น Vercel

--- src/constants/docsList.jsx
+++ src/constants/docsList.jsx
@@ -1,7 +1,7 @@
 export const docsList = [
   {
     id: "DocsPageDevfolio",
-    title: "Portfolio Seminar + Deploy ขึ้น Vercel",
+    title: "Devfolio Seminar + Deploy ขึ้น Vercel",
     desc: "อธิบายขั้นตอนการพัฒนา Portfolio ด้วย React, TailwindCSS และ Motion พร้อมสอนการใช้งาน Git, GitHub และการ Deploy เว็บไซต์ขึ้น Vercel",
     image: "/PortfolioSeminar.png",
     tags: ["React", "TailwindCSS", "Motion", "Git", "GitHub", "Vercel"],
