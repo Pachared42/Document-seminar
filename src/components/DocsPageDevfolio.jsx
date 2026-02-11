09dcd9518d06a176c55a1415edc0c368f6199457
Axm.pachara <pachared.amr12@gmail.com>
2026-02-12 02:47:50 +0700

    Update DocsPageDevfolio.jsx

--- src/components/DocsPageDevfolio.jsx
+++ src/components/DocsPageDevfolio.jsx
@@ -16,8 +16,7 @@ function DocsPageDevfolio() {
             Devfolio Seminar + Deploy ขึ้น Vercel
           </h1>
           <p className="text-gray-400 mt-2 mb-6">
-            คู่มือการติดตั้งและจัดโครงสร้างโปรเจกต์ Devfolio
-            ตั้งแต่เริ่มต้น
+            คู่มือการติดตั้งและจัดโครงสร้างโปรเจกต์ Devfolio ตั้งแต่เริ่มต้น
           </p>
           <img
             className="mb-4 rounded-2xl border border-[#1f2330]"
@@ -653,7 +652,7 @@ export const SOCIAL_MEDIA_LINKS = [
   },
 ];`}</CodeBlock>
 
-<h3 className="font-semibold text-lg mt-6 text-slate-100">
+          <h3 className="font-semibold text-lg mt-6 text-slate-100">
             index.css
           </h3>
           <CodeBlock>{`@import url('https://fonts.googleapis.com/css2?family=Noto+Sans+Thai:wdth,wght@62.5..100,100..900&family=Noto+Sans:ital,wdth,wght@0,62.5..100,100..900;1,62.5..100,100..900&display=swap');
@@ -671,7 +670,7 @@ body {
 import { NAVIGATION_LINKS } from '../constants/constants';
 import { FaTimes } from 'react-icons/fa';
 import { FaBars } from 'react-icons/fa6';
-import { AnimatePresence, motion } from 'motion/react';
+import { AnimatePresence, motion } from 'framer-motion';
 
 function Navbar() {
     const [isMobileMenuOpen, setIsMobileMenuOpen] = useState(false);
@@ -2131,70 +2130,6 @@ function App() {
 }
 
 export default App;`}</CodeBlock>
-
-          <p className="mt-6 text-slate-100">ตัวอย่าง Profile Github README</p>
-          <img
-            className="w-full rounded-2xl mt-6 border border-[#1f2330]"
-            src="/DocsPageDevfolio/ProfileGithubREADME.png"
-            alt="username Github"
-          />
-
-          <h3 className="font-semibold text-lg mt-6 text-slate-100">
-            README.md
-          </h3>
-          <CodeBlock>{`<p align="center">
-  <img src="imageREADME.jpg" width="100%" />
-</p>
-
-Hi ![](https://user-images.githubusercontent.com/18350557/176309783-0785949b-9127-417c-8b55-ab5a4333674e.gif) My name is Pachara Kalapakdee
-===========================================================================================================================================
-
-### Front-End Developer | Passionate about UI/UX & Modern Web
-
-* 🌍  I'm based in Suphan Buri
-* 🖥️  See my portfolio at [Pachara](http://pachara.vercel.app/)
-* ✉️  You can contact me at [pachared.amr12@gmail.com](mailto:pachared.amr12@gmail.com)
--------------------
-# </ Skills >
-### Front-End
-<p align="left">
-<a href="https://developer.mozilla.org/en-US/docs/Glossary/HTML5" target="_blank" rel="noreferrer"><img src="skills/html5.svg" width="50" height="50" /></a>&nbsp;
-<a href="https://www.w3.org/TR/CSS/#css" target="_blank" rel="noreferrer"><img src="skills/css.svg" width="50" height="50" /></a>&nbsp;
-<a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank" rel="noreferrer"><img src="skills/javascript.svg" width="50" height="50" /></a>&nbsp;
-<a href="https://vitejs.dev/" target="_blank" rel="noreferrer"><img src="skills/vite.svg" width="50" height="50" /></a>&nbsp;
-<a href="https://reactjs.org/" target="_blank" rel="noreferrer"><img src="skills/react_dark.svg" width="50" height="50" /></a>&nbsp;
-<a href="https://nextjs.org/" target="_blank" rel="noreferrer"><img src="skills/nextjs_icon_dark.svg" width="50" height="50" /></a>&nbsp;
-<a href="https://tailwindcss.com/" target="_blank" rel="noreferrer"><img src="skills/tailwindcss.svg" width="50" height="50" /></a>&nbsp;
-<a href="https://mui.com/" target="_blank" rel="noreferrer"><img src="skills/materialui.svg" width="50" height="50" /></a>&nbsp;
-</p>
-
-### Back-End
-<p align="left">
-<a href="https://go.dev/doc/" target="_blank" rel="noreferrer"><img src="skills/golang_dark.svg" width="50" height="50" /></a>&nbsp;
-<a href="https://www.php.net/" target="_blank" rel="noreferrer"><img src="skills/php_dark.svg" width="50" height="50" /></a>
-</p>
-
-### Database
-<p align="left">
-<a href="https://www.mysql.com/" target="_blank" rel="noreferrer"><img src="skills/mysql-wordmark-dark.svg" width="60" height="60" /></a>&nbsp;
-</p>
-
-### Dev Tools
-<p align="left">
-<a href="https://code.visualstudio.com/" target="_blank" rel="noreferrer"><img src="skills/vscode.svg" width="50" height="50" /></a>&nbsp;
-<a href="https://cursor.com/home?from=agents" target="_blank" rel="noreferrer"><img src="skills/cursor_dark.svg" width="50" height="50" /></a>&nbsp;
-<a href="https://git-scm.com/" target="_blank" rel="noreferrer"><img src="skills/git.svg" width="50" height="50" /></a>&nbsp;
-<a href="https://github.com/" target="_blank" rel="noreferrer"><img src="skills/github_dark.svg" width="50" height="50" /></a>&nbsp;
-<a href="https://www.postman.com/" target="_blank" rel="noreferrer"><img src="skills/postman.svg" width="50" height="50" /></a>&nbsp;
-<a href="https://www.warp.dev/" target="_blank" rel="noreferrer"><img src="skills/warp.svg" width="50" height="50" /></a>&nbsp;
-<a href="https://n8n.io/" target="_blank" rel="noreferrer"><img src="skills/n8n.svg" width="50" height="50" /></a>&nbsp; -->
-</p>
-
-### Design Tools
-<p align="left">
-<a href="https://www.adobe.com/uk/products/photoshop.html" target="_blank" rel="noreferrer"><img src="skills/photoshop.svg" width="50" height="50" /></a>&nbsp;
-<a href="https://www.adobe.com/uk/products/illustrator.html" target="_blank" rel="noreferrer"><img src="skills/illustrator.svg" width="50" height="50" /></a>
-</p>`}</CodeBlock>
         </Section>
 
         <footer className="text-center text-slate-500 mt-10">
